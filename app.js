ca84003c2aba86e80ce1a04f25914bc874660382
Adam Olszewski <adamolszewski96@gmail.com>
2023-06-07 18:33:01 +0200

    add about/contact pages linked to app.js

--- app.js
+++ app.js
@@ -16,11 +16,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", (req,res)=>{
+  res.render("home", {
+    home: homeStartingContent
+  });
+});
 
+app.get("/about", (req,res)=>{
+  res.render("about", {
+    about: aboutContent
+  });
+});
 
-
-
-
+app.get("/contact", (req,res)=>{
+  res.render("contact", {
+    contact: contactContent
+  });
+});
 
 
 
